2767b08dfeda1a24377ccc0c5299ec0dc809e7c2
TheFireBlast <fireblast901@gmail.com>
2024-11-21 21:22:21 -0300

    feat: add gui para AssetDict

---
+++ Assets/Editor/Scripts/AssetDictItemDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(AssetDictItem))]
+public class AssetDictItemDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 16f * 2;
+    }
+    public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+    {
+        var nameProp = property.FindPropertyRelative("name");
+        var valueProp = property.FindPropertyRelative("value");
+
+        EditorGUIUtility.wideMode = true;
+        EditorGUIUtility.labelWidth = 70;
+        rect.height /= 2;
+        nameProp.stringValue = EditorGUI.TextField(rect, nameProp.stringValue);
+        rect.y += rect.height;
+        EditorGUI.ObjectField(rect, valueProp);
+    }
+}
